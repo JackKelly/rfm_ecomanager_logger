66603ea2e645983253e609ec47d60f2d066e9f2d
Jack Kelly <jack-list@xlk.org.uk>
2013-01-22 20:00:57 +0000

    Changing PyPI name to rfm-ecomanager-logger because underscores do bad thing with PyPI: http://mail.python.org/pipermail/distutils-sig/2011-August/017935.html

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "rfm_ecomanager_logger",
+    name = "rfm-ecomanager-logger",
     version = "0.1dev",
     packages = find_packages(),
     install_requires = ['pickle'],
