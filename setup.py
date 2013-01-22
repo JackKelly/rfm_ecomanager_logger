30fa88c64189fb08c4e68d490178ec86816e98e5
Jack Kelly <jack-list@xlk.org.uk>
2013-01-22 19:51:10 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "rfm_ecomanager_logger",
+    version = "0.1dev",
+    packages = find_packages(),
+    install_requires = ['pickle'],
+    author = "Jack Kelly",
+    author_email = "jack-list@xlk.org.uk",
+    description = "Log power data from rfm_edf_ecomanager running on a Nanode (or similar)",
+    license = "MIT",
+    keywords = "power logging python",
+    url = "https://github.com/JackKelly/rfm_ecomanager_logger/",
+    download_url = "https://github.com/JackKelly/rfm_ecomanager_logger/tarball/master",
+    long_description = open('README.md').read()
+)
