d5bb5706a41af6f482d03d73de8d879342a4041c
Jack Kelly <jack-list@xlk.org.uk>
2013-01-24 14:49:16 +0000

    done start command. need to test

--- lm.sh
+++ lm.sh
@@ -13,30 +13,78 @@ name[2]="BABYSITTER"
 
 length=${#name[@]} # length of array `name`
 
-# create a `directory` array with the actual directoryies
-for (( i=0; i<$length; i++ ))
-do
-    subst="${name[$i]}_DIR"
-    directory[$i]="${!subst}"
-done
+print_error()
+{
+    echo -e $1 >/dev/stderr
+}
 
-# sanity checks
-echo "Running sanity checks..."
-for (( i=0; i<$length; i++ ))
-do
-    if [ -z "$directory[$i]" ]
+check_return_value ()
+{
+    if [ $? -ne 0 ]
     then
-	echo "* ERROR: ${name[$i]} directory is not set!"
+	print_error "ERROR. lm.sh script cannot finished. Please see above for error details."
+	exit $?
+    fi
+}
+
+
+check_directory ()
+# Arguments: 1) path 2) variable name
+{
+    if [ -z "$1" ]
+    then
+	print_error "\n* ERROR: \$$2 directory is not set!\n"
 	exit 1
     else
 	if [ -d "${directory[$i]}" ]
 	then
-	    echo "* ${name[$i]}_DIR set to ${directory[$i]} which is a valid directory"
+	    echo "* \$$2 set to $1 which is a valid directory"
 	else
-	    echo "* ERROR: ${name[$i]}_DIR set to ${directory[$i]} WHICH IS NOT A DIRECTORY"
+	    print_error "\n* ERROR: \$$2 set to $1 WHICH IS NOT A DIRECTORY\n"
 	    exit 1
 	fi
+    fi    
+}
+
+
+check_process()
+# Arguments: 1) process name
+{
+    check_return_value
+    pgrep $1
+    if [ $? -ne 0 ]
+    then
+	print_error "\nERROR: $1 failed to start!\n"
+	return 1
+    fi   
+    return 0
+}
+
+
+start_process ()
+# Arguments: 1) directory, 2) file within directory, 3) process name
+{
+    echo "Starting $1$2..."
+    cd $1
+    nohup $2 &
+    check_process $3
+    if [ $? -ne 0 ]
+    then
+	tail nohup.out >/dev/stderr
+	echo ""
+	exit 1
     fi
+    echo "Successfully started $1$2"
+}
+
+
+# create a `directory` array with the actual directories and sanity check
+echo "Running sanity checks..."
+for (( i=0; i<$length; i++ ))
+do
+    subst="${name[$i]}_DIR"
+    directory[$i]="${!subst}"
+    check_directory "${directory[$i]}" "${name[$i]}_DIR"
 done
 echo -e "...all sanity checks passed!\n"
 
@@ -49,14 +97,27 @@ case "$1" in # switch on the first command
 	    echo ""
 	    echo "Updating ${name[$i]}"
 	    cd ${directory[$i]} && git pull
-	    if [ $? -ne 0 ]
-	    then
-		echo "ERROR. lm.sh script cannot finished. Please see above for error details."
-		exit 1
-	    fi
+	    check_return_value
 	done
 	;;
 
+"flash")
+	echo "Flashing Nanode with latest code from github"
+	check_directory "$RFM_EDF_ECOMANAGER_DIR" "RFM_EDF_ECOMANAGER_DIR"
+	echo "First, updating code from github:"
+	cd "$RFM_EDF_ECOMANAGER_DIR" && git pull
+	check_return_value
+	echo "Running flash.sh script:"
+	./flash.sh
+	check_return_value
+	;;
+
+"start")
+	start_process "$RFM_ECOMANAGER_LOGGER_DIR" "/rfm_ecomanager_logger/rfm_ecomanager_logger.py" "rfm_ecomanager_logger.py"
+	start_process "$BABYSITTER_DIR" "/babysitter/babysitter.py" "babysitter.py"
+	;;
+
 esac # end of case
+
 
 echo -e "\nAll done\n"
